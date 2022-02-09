0fb8871574d3eff2eee9c535cef9c638f0694c53
Marius Wallin <post@mariuswallin.no>
2022-02-09 12:08:24 +0100

    react player

---
+++ react_player/src/App.js
@@ -0,0 +1,114 @@
+import "./styles/css/main.css";
+
+export default function App() {
+  return (
+    <main>
+      <nav>
+        <ul className="flex justify-between items-center list-style-none">
+          <li>
+            <img alt="arrow" className="icon-small" src="/arrow.svg" />
+          </li>
+          <li className="text-gray-600 text-xl font-semibold">Now playing</li>
+          <li>
+            <img alt="dots" className="icon" src="/dots.svg" />
+          </li>
+        </ul>
+      </nav>
+      <div id="screen">
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+      <h2 className="text-center mt-6">#02 - Practice</h2>
+      <p className="text-center text-gray-400 text-sm font-semibold mt-2">
+        Digital Marketing - By Setup Cast
+      </p>
+      <div
+        className="flex mt-4 items-center justify-center w-full text-md"
+        id="bar"
+      >
+        <p className="text-gray-400 font-semibold">4.10</p>
+        <div id="slider">
+          <div style={{ width: "50%" }}></div>
+          <div style={{ left: "50%" }}></div>
+        </div>
+        <p className="text-gray-400 font-semibold">12:02</p>
+      </div>
+      <div className="flex items-center justify-center" id="actions">
+        <button type="button" id="rewind">
+          <img alt="dots" className="icon" src="/rewind.svg" />
+        </button>
+        <button type="button" id="play">
+          <img alt="play" className="icon" src="/play.svg" />
+        </button>
+        <button type="button" id="forward">
+          <img alt="forward" className="icon" src="/forward.svg" />
+        </button>
+      </div>
+      <div id="history">
+        <h3 className="font-semibold text-xl text-gray-600">Recently played</h3>
+        <ul className="list-style-none">
+          <li className="flex items-center">
+            <div></div>
+            <div className="flex items-center">
+              <p>
+                <span className="font-semibold text-lg">
+                  #01 - Start with SEO
+                </span>
+                <span className="text-sm text-gray-500">
+                  By Setup Cast - 15:35
+                </span>
+              </p>
+              <button type="button">
+                <img alt="play" className="icon" src="/play.svg" />
+              </button>
+            </div>
+          </li>
+          <li className="flex items-center">
+            <div></div>
+            <div className="flex items-center">
+              <p>
+                <span className="font-semibold text-lg">
+                  #01 - Start with SEO
+                </span>
+                <span className="text-sm text-gray-500">
+                  By Setup Cast - 15:35
+                </span>
+              </p>
+              <button type="button">
+                <img alt="play" className="icon" src="/play.svg" />
+              </button>
+            </div>
+          </li>
+          <li className="flex items-center">
+            <div></div>
+            <div className="flex items-center">
+              <p>
+                <span className="font-semibold text-lg">
+                  #01 - Start with SEO
+                </span>
+                <span className="text-sm text-gray-500">
+                  By Setup Cast - 15:35
+                </span>
+              </p>
+              <button type="button">
+                <img alt="play" className="icon" src="/play.svg" />
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
---
+++ react_player/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import { MainHeading } from '../styles/styled/Styles'
+
+function MyComponent() {
+  return (
+    <div>
+      <MainHeading>Component</MainHeading>
+      <button type="button">Test</button>
+    </div>
+  )
+}
+
+export default MyComponent
