bd5890fa33657b35ac68f97ab14583823be0c9f2
Marius Wallin <post@mariuswallin.no>
2022-03-02 13:53:17 +0100

    live pause 2

--- uke8_3/src/components/Form.js
+++ uke8_3/src/components/Form.js
@@ -1,3 +1,40 @@
+import { useState } from 'react'
+
 export default function Form() {
-  return <form>Form</form>
+  const [comment, setComment] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [success, setSuccess] = useState(false)
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value)
+  }
+
+  const addComment = (event) => {
+    event.preventDefault()
+    setLoading(true)
+    setTimeout(() => {
+      setSuccess(true)
+      setLoading(false)
+    }, 2000)
+    // console.log(comment)
+  }
+
+  return (
+    <form method="POST" onSubmit={addComment}>
+      <h2>Legg igjen en kommentar</h2>
+      <label htmlFor="comment">
+        Kommentar
+        <textarea
+          id="comment"
+          name="comment"
+          cols={15}
+          rows={5}
+          onChange={handleCommentChange}
+          value={comment}
+        />
+      </label>
+      <button type="submit">{loading ? 'Sender ...' : 'Send'}</button>
+      {success ? <p>Kommentar sendt</p> : null}
+    </form>
+  )
 }
--- uke8_3/src/components/Navigation.js
+++ uke8_3/src/components/Navigation.js
@@ -1,3 +1,22 @@
 export default function Navigation() {
-  return <nav>Navigation</nav>
+  return (
+    <header>
+      <nav>
+        <a href="#" id="logo">
+          Logo
+        </a>
+        <ul>
+          <li>
+            <a href="#">Hjem</a>
+          </li>
+          <li>
+            <a href="#">Produkter</a>
+          </li>
+          <li>
+            <a href="#">Tjenester</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
